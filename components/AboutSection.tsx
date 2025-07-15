2e8ab9414601f1a2245e4166e0b7343f8db1b2e0
mohd kaif <mk2818356@gmail.com>
2025-07-16 02:41:56 +0530

    fix one more error

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -1,7 +1,7 @@
 
 "use client"
 import { motion } from "framer-motion"
-import { Code, Palette, Zap, Users, Award, Coffee } from "lucide-react"
+import {  Palette, Zap } from "lucide-react"
 
 export function AboutSection() {
   const skills = [
