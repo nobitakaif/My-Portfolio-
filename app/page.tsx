61a3b5d4e1b2d6121f8a62f017491f4189dcd477
mohd kaif <mk2818356@gmail.com>
2025-08-21 13:35:22 +0530

    fixes build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import { AboutSection } from "@/components/AboutSection";
-import { Header } from "@/components/Header";
+
 import HeaderProvider from "@/components/HeaderProvider";
 import { HeroSection } from "@/components/MainContent";
 import { ProjectsSection } from "@/components/Projects";
-import { SessionProvider } from "next-auth/react";
+
 
 
 
